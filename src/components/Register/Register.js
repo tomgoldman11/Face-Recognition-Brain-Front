11e827b4303c2a105ae67ec4c7a2f7690851fbe9
Tom Goldman <tomgoldman11@gmail.com>
2019-10-25 18:07:15 +0300

    adding fetches from heroku url

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://still-bayou-26471.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
